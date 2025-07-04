50a8e8eaf494c2798a23a92cfc48888b57e80721
ameer akech <ameerakech@gmail.com>
2025-07-04 19:06:41 +0300

    created a signup page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,12 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.compose.material3:material3:1.3.0")
+    implementation("androidx.navigation:navigation-compose:2.8.3")
+    implementation("androidx.compose.ui:ui:1.7.4")
+    implementation("androidx.compose.runtime:runtime:1.7.4")
+    implementation("androidx.compose.foundation:foundation:1.7.4")
+
 }
