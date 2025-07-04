929e1ef55b4867474bb318d7ffc02958528bc48e
Shabach Muriithi <shabachmuriithi@gmail.com>
2025-07-04 18:33:31 +0300

    added navigation file

--- app/src/main/java/imperial/mobile/irecycle/NavGraph.kt
+++ app/src/main/java/imperial/mobile/irecycle/NavGraph.kt
@@ -14,6 +14,25 @@ fun AuthNavGraph(navController: NavHostController = rememberNavController()) {
         composable("welcome") {
             WelcomeScreen(onNextClick = { navController.navigate("how_it_works") })
         }
+        composable("how_it_works") {
+            HowItWorksScreen(
+                onBackClick = { navController.popBackStack() },
+                onNextClick = { navController.navigate("login") }
+            )
+        }
 
+        // Authentication Screens
+        composable("login") {
+            LoginScreen(
+                onSignUpClick = { navController.navigate("signup") },
+                onForgotPasswordClick = { navController.navigate("forgot_password") }
+            )
+        }
+        composable("signup") {
+            SignUpScreen(onLoginClick = { navController.navigate("login") })
+        }
+        composable("forgot_password") {
+            ForgotPasswordScreen(onBackToLoginClick = { navController.navigate("login") })
+        }
     }
 }
